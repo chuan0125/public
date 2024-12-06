5ae1a6964158176caaf6f3eb7f25b74052065ba5
chuan0125 <64683283+chuan0125@users.noreply.github.com>
2024-12-06 16:17:36 +0800

    Update r2y.sh

--- r2y.sh
+++ r2y.sh
@@ -1,5 +1,5 @@
-apt update && apt install wget unzip -y && \
+apt update && apt install wget unzip openssh-server -y && \
 wget https://github.com/v2fly/v2ray-core/releases/download/v5.14.0/v2ray-linux-64.zip && \
-unzip v2ray-linux-64.zip && \
-echo '{"inbound":{"port":9999,"protocol":"vmess","settings":{"clients":[{"id":"54f0f20d-7ff9-4dbd-ada0-fa1e27622d85","level":1,"alterId":64}]}}, "streamSettings":{"network":"tcp"},"outbound":{"protocol":"freedom","settings":{}},"outboundDetour":[{"protocol":"blackhole","settings":{},"tag":"blocked"}]}' > ./config.json && \
-v2ray run -c ./config.json
+unzip /v2ray-linux-64.zip && \
+echo '{"inbound":{"port":9999,"protocol":"vmess","settings":{"clients":[{"id":"54f0f20d-7ff9-4dbd-ada0-fa1e27622d85","level":1,"alterId":64}]}}, "streamSettings":{"network":"tcp"},"outbound":{"protocol":"freedom","settings":{}},"outboundDetour":[{"protocol":"blackhole","settings":{},"tag":"blocked"}]}' > /config.json && \
+/v2ray run -c /config.json
